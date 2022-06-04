cd4cca48751739b1388f27185c77889b3beb3520
burik84 <avburik84@gmail.com>
2022-06-05 01:21:33 +0300

    faet: ass solution fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,24 @@
+import { throws } from 'assert';
+import * as fs from 'fs/promises';
+import path from 'path';
 export const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const filename = path.join(path.resolve(), '/src/fs/files/fresh.txt');
+
+  try {
+    await fs.access(filename);
+    // throw Error('FS operation failed')
+    console.error('FS operation failed');
+  } catch {
+    try {
+        await fs.writeFile(filename, content);
+        console.log('FS operation finished');
+      } catch (error) {
+        console.error(error)
+      }
+  }
+
+
 };
+
+create();
