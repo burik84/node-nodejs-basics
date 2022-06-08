93fc1b7c05751152672d49d304b2dfecf5374679
burik84 <avburik84@gmail.com>
2022-06-08 23:38:01 +0300

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const inputProcess = process.argv.slice(2);
+  const cliProcess = inputProcess.reduce((acc, cur, idx, arr) => {
+    const item = arr[idx + 1];
+    if (item && cur.startsWith('--')) {
+      const str = cur.slice(2);
+      const template = `${str} is ${item}`;
+      acc.push(template);
+    }
+    return acc;
+  }, []);
+
+  console.log(cliProcess.join(', '));
 };
+
+parseArgs();
