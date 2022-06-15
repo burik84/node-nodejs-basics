7fb00e92de964e638961145255e3f6663f94548d
burik84 <avburik84@gmail.com>
2022-06-16 00:37:57 +0300

    node: fix hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,21 +1,19 @@
-
-import { createHash } from "crypto";
-import {readFile} from 'fs/promises';
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import path from 'path';
 export const calculateHash = async () => {
-    const __fileName = fileURLToPath(import.meta.url);
-    const __dirName = dirname(__fileName);
-    const fileToHash='/files/fileToCalculateHashFor.txt'
+  const __fileName = fileURLToPath(import.meta.url);
+  const __dirName = path.dirname(__fileName);
+  const fileToHash = path.join(__dirName, '/files/fileToCalculateHashFor.txt');
 
-    try {
-        const fileContent=await readFile(`${__dirName}${fileToHash}`)
-        const hash=createHash('sha256').update(fileContent).digest('hex')
-        console.log('Hash: ', hash);
-    } catch (error) {
-        throw error
-    }
-
+  try {
+    const fileContent = await readFile(`${fileToHash}`);
+    const hash = createHash('sha256').update(fileContent).digest('hex');
+    console.log('Hash: ', hash);
+  } catch (error) {
+    throw error;
+  }
 };
 
-calculateHash()
+calculateHash();
