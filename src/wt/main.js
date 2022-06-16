2b63219f37648f614c1f1354d49a048ebb36c487
burik84 <avburik84@gmail.com>
2022-06-16 23:44:01 +0300

    node:add workers

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,35 @@
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
 export const performCalculations = async () => {
-    // Write your code here
+  const cp = cpus();
+  let number = 10;
+  const name = path.join(__dirName, '/worker.js');
+
+  const workerResult = await Promise.allSettled(
+    cp.map(() => {
+      return new Promise((resolve, reject) => {
+        const worker = new Worker(name, {
+          workerData: number++,
+        });
+        worker.on('message', (msg) => resolve(msg));
+        worker.on('error', (msg) => reject(msg));
+      });
+    })
+  );
+  const result = workerResult.map(({ status, value }) => ({
+    status: status === 'fulfilled' ? 'resolved' : 'error',
+    data: status === 'fulfilled' ? value : null,
+  }));
+
+  console.log(result);
+
+  return result;
 };
+
+performCalculations();
