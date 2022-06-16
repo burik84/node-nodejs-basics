0978a42412d194161118464ef7f1cde9e56917f1
burik84 <avburik84@gmail.com>
2022-06-17 00:20:51 +0300

    node:add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,27 @@
+import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = dirname(__fileName);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const name = path.join(__dirName, '/files/script.js');
+  let init = false;
+
+  const child_process = spawn('node',[ name, ...args.split(' ')]);
+
+  process.stdin.on('data', (msg) => {
+    child_process.stdin.write(msg);
+  });
+
+  child_process.stdout.on('data', (data) => {
+    console.log(data.toString());
+    if (!init) {
+      console.log('Write anything to console... \n');
+      init = true;
+    }
+  });
 };
+
+spawnChildProcess('--silent --all');
