81108c9c67f3446b5822d14119007cca44fe7041
burik84 <avburik84@gmail.com>
2022-06-21 22:44:45 +0300

    feat: add add

--- src/handlers/add.js
+++ src/handlers/add.js
@@ -1,3 +1,16 @@
-export const  handleAdd=()=>{
+import { open } from 'fs/promises';
+import { resolve } from 'path';
+import displayCurrentDirectory from '../helpers/display';
 
-}
+export const handleAdd = async ([newName]) => {
+  let fileHandle;
+  try {
+    const pathToFile = resolve(process.cwd(), newName);
+    fileHandle = await open(pathToFile, 'w');
+    displayCurrentDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  } finally {
+    fileHandle?.close();
+  }
+};
