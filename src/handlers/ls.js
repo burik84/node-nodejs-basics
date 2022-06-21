d63cd377cae14d17f8d1bab1b3bda3403f8bc9f6
burik84 <avburik84@gmail.com>
2022-06-22 00:22:04 +0300

    feat: add ls

--- src/handlers/ls.js
+++ src/handlers/ls.js
@@ -1,3 +1,14 @@
-export const  handleLS=()=>{
-
-}
+import { table } from 'console';
+import { readdir } from 'fs/promises';
+import { resolve } from 'path';
+import displayCurrentDirectory from '../helpers/display';
+export const handleLS = async () => {
+  try {
+    const currentDir = resolve(process.cwd());
+    const files = await readdir(currentDir);
+    console.table(files);
+    displayCurrentDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  }
+};
