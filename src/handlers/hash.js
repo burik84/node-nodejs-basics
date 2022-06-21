e4922b44085c72537daf9322e41d1ce15c8aebde
burik84 <avburik84@gmail.com>
2022-06-22 00:18:03 +0300

    feat: add hash

--- src/handlers/hash.js
+++ src/handlers/hash.js
@@ -1,3 +1,18 @@
-export const  handleHash=()=>{
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+import { pipeline } from 'stream/promises';
+import { customOutput } from '../helpers/output';
+import displayCurrentDirectory from '../helpers/display';
+export const handleHash = async ([rawPath]) => {
+  try {
+    const pathToFile = resolve(rawPath);
+    const hsh = createHash('sha256');
+    const readeblStream = createReadStream(pathToFile);
 
-}
+    await pipeline(readeblStream, hsh.setEncoding('hex'), customOutput());
+    displayCurrentDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  }
+};
