5ed6db743e506093e83fa5b200df19d868f268e4
burik84 <avburik84@gmail.com>
2022-06-22 00:34:57 +0300

    feat: change mv

--- src/handlers/mv.js
+++ src/handlers/mv.js
@@ -1,3 +1,25 @@
-export const  handleMv=()=>{
+import { createReadStream, createWriteStream } from 'fs';
+import { unlink } from 'fs/promises';
+import { parse, resolve } from 'path';
+import { pipeline } from 'stream/promises';
+import { displayCurrentDirectory } from '../helpers/display';
+import { isDirectory as isCurrentDirectory } from '../helpers/isFiles';
+export const handleMv = async ([rawPath, rawPathNew]) => {
+  try {
+    const isDirectory = await isCurrentDirectory(rawPathNew);
+    if (!isDirectory) throw new Error("It's not a directory");
 
-}
+    const pathToFile = resolve(rawPath);
+    const { base } = parse(pathToFile);
+
+    const pathToNewDirectory = resolve(rawPathNew, base);
+
+    const readeblStream = createReadStream(pathToFile);
+    const writebleStream = createWriteStream(pathToNewDirectory);
+    await pipeline(readeblStream, writebleStream);
+    await unlink(pathToFile);
+    displayCurrentDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  }
+};
