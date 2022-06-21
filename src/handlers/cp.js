546fa46201c09fb8e064a4269bc72f813fb53e61
burik84 <avburik84@gmail.com>
2022-06-21 23:29:06 +0300

    feat: add copy

--- src/handlers/cp.js
+++ src/handlers/cp.js
@@ -1,3 +1,19 @@
-export const  handleCP=()=>{
+import { createReadStream, createWriteStream } from 'fs';
+import { parse, resolve } from 'path';
+import { pipeline } from 'stream/promises';
+import { displayCurrentDirectory } from '../helpers/display';
+export const handleCP = async ([rawPath, rawPathToDirectory]) => {
+  try {
+    const pathToFile = resolve(rawPath);
+    const { base } = parse(pathToFile);
+    const pathToDirectory = resolve(rawPathToDirectory, base);
 
-}
+    const readeblStream = createReadStream(pathToFile);
+    const writebleStream = createWriteStream(pathToDirectory);
+
+    await pipeline(readeblStream, writebleStream);
+    displayCurrentDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  }
+};
