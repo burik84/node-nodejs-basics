539452eae692fd725f9808a62ba0521a576ff97c
burik84 <avburik84@gmail.com>
2022-06-21 23:00:20 +0300

    --no-edit

--- src/handlers/cd.js
+++ src/handlers/cd.js
@@ -1,3 +1,10 @@
-export const  handleCD=()=>{
+import { displayCurrentDirectory } from '../helpers/display';
 
-}
+export const handleCD = async ([rawPath]) => {
+  try {
+    process.chdir(rawPath);
+    displayCurrentDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  }
+};
